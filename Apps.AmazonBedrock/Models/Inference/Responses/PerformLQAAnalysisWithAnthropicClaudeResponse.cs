3a20846cbf0120bf387d0ef70640244e9c9b50a3
sokolenkoolha <olha.sokolenko@blackbird.io>
2023-10-27 12:03:06 +0300

    added lqa analysis action

---
+++ Apps.AmazonBedrock/Models/Inference/Responses/PerformLQAAnalysisWithAnthropicClaudeResponse.cs
@@ -0,0 +1,12 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.AmazonBedrock.Models.Inference.Responses;
+
+public class PerformLQAAnalysisWithAnthropicClaudeResponse
+{
+    [Display("LQA analysis")]
+    public string LQAAnalysis { get; set; }
+    
+    [Display("Corrected translation")]
+    public string CorrectedTranslation { get; set; }
+}
