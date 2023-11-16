495f47709ac6b8e2c0ed85a7e0d29d1813859e5e
sokolenkoolha <olha.sokolenko@blackbird.io>
2023-11-16 18:47:06 +0200

    added generate text with Meta Llama 2 action

---
+++ Apps.AmazonBedrock/DataSourceHandlers/ModelHandlers/MetaLlamaModelDataSourceHandler.cs
@@ -0,0 +1,13 @@
+﻿using Blackbird.Applications.Sdk.Common.Invocation;
+
+namespace Apps.AmazonBedrock.DataSourceHandlers.ModelHandlers;
+
+public class MetaLlamaModelDataSourceHandler : ModelBaseDataSourceHandler
+{
+    protected override string Provider => "meta";
+    protected override string Modality => "TEXT";
+    
+    public MetaLlamaModelDataSourceHandler(InvocationContext invocationContext) : base(invocationContext)
+    {
+    }
+}
---
+++ Apps.AmazonBedrock/Models/Inference/Responses/RunInferenceWithMetaLlamaResponse.cs
@@ -0,0 +1,9 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.AmazonBedrock.Models.Inference.Responses;
+
+public class RunInferenceWithMetaLlamaResponse
+{
+    [Display("Generated text")]
+    public string Generation { get; set; }
+}
